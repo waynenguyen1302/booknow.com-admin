9d7925f7b8c56a33993251ba91999f7f8ebb003b
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2023-02-13 22:54:10 -0800

    added api

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
         e.preventDefault();
         dispatch({type: "LOGIN_START"});
         try {
-            const res = await axios.post("/auth/login", credentials);            
+            const res = await axios.post("https://booknow-com-api.onrender.com/auth/login", credentials);            
             if(res.data.isAdmin) {
               dispatch({type: "LOGIN_SUCCESS", payload: res.data.details});
               navigate("/");
